93200a260ef134e66f545766b0938a01c93559e9
fcamposp <fabrycio7campos7@gmail.com>
2019-06-04 12:41:48 -0600

    modificacion en archivo base y creacion de nuevos templates en proyecto

--- apps/proyecto/urls.py
+++ apps/proyecto/urls.py
@@ -7,5 +7,9 @@ from django.contrib.auth.views import login
 app_name="proyectos"
 urlpatterns = [
     url(r'^busquedaProyecto/$',login_required(buscarProyecto.as_view()),name="busquedaProyecto"),
+    url(r'^edicionProyecto/$',login_required(editarProyecto.as_view()),name="editarProyecto"),
+    url(r'^asignacionRecurso/$',login_required(asignacionRecurso.as_view()),name="asignacionRecurso"),
+    url(r'^detalleProyecto/$',login_required(detalleProyecto.as_view()),name="detalleProyecto"),
+    url(r'^solicitarRecursos/$',login_required(solicitarRecursos.as_view()),name="solicitarRecursos"),
 
 ]
--- apps/proyecto/views.py
+++ apps/proyecto/views.py
@@ -4,3 +4,18 @@ from django.views.generic import TemplateView
 
 class buscarProyecto(TemplateView):
 	template_name='proyecto/BuscarProyecto.html'
+
+
+class asignacionRecurso(TemplateView):
+	template_name='proyecto/AsignacionRecurso.html'
+
+
+class editarProyecto(TemplateView):
+	template_name='proyecto/EditarProyecto.html'
+
+class detalleProyecto(TemplateView):
+	template_name='proyecto/DetalleProyecto.html'
+
+class solicitarRecursos(TemplateView):
+	template_name='proyecto/SolicitarRecursos.html'
+
